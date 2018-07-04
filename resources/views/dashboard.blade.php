72bcad74377d45e8d4d37335fde0b0f0d2043f50
JorgeEnriquez1994 <enriquezrodriguezjorgeangel@gmail.com>
2018-07-04 11:08:46 -0500

    create login and dashboard
    - Create achives login and dashboard on the folder Views.
    - Comunicate the archives whit yours respective templates.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('templates.mastertemplate')
+@section('content-master')
+<div class="row">
+   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+   <h2>Dashboard</h2>
+   </div>
+</div>
+@stop
---
+++ resources/views/templates/logintemplate.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Mexicanal | Login</title>
+		<link rel="stylesheet" href="">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> 
+	</head>
+
+	<body>
+        <div class="container-fluid">
+            @yield('content-login')
+        </div>
+		<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+	</body>
+</html>
