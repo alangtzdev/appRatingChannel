a7209ce323d280ae4d2e0d4e354b2245efecb63d
JorgeEnriquez1994 <enriquezrodriguezjorgeangel@gmail.com>
2018-07-13 16:40:40 -0500

    change js and css

--- resources/views/templates/logintemplate.blade.php
+++ resources/views/templates/logintemplate.blade.php
@@ -12,20 +12,14 @@
       <!-- BEGIN GLOBAL MANDATORY STYLES -->
       <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
       <link rel="stylesheet" href="{{'css/globalMandatoryStyle.css'}}">
-      {{-- <link href="{{asset('assets/global/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css"/>
+      <link href="{{asset('assets/global/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css"/>
       <link href="{{asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css')}}" rel="stylesheet" type="text/css"/>
-      <link href="{{asset('assets/global/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-      <link href="{{asset('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css')}}" rel="stylesheet" type="text/css" /> --}}
       <!-- END GLOBAL MANDATORY STYLES -->
       <!-- BEGIN PAGE LEVEL PLUGINS -->
       <link rel="stylesheet" href="{{'css/pageLevelPluginStyle.css'}}">
-      {{-- <link href="{{asset('assets/global/plugins/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
-      <link href="{{asset('assets/global/plugins/select2/css/select2-bootstrap.min.css')}}" rel="stylesheet" type="text/css" /> --}}
       <!-- END PAGE LEVEL PLUGINS -->
       <!-- BEGIN THEME GLOBAL STYLES -->
       <link rel="stylesheet" href="{{'css/themeGlobalStyle.css'}}">
-      {{-- <link href="{{asset('assets/global/css/components-md.min.css')}}" rel="stylesheet" id="style_components" type="text/css" />
-      <link href="{{asset('assets/global/css/plugins-md.min.css')}}" rel="stylesheet" type="text/css" /> --}}
       <!-- END THEME GLOBAL STYLES -->
       <!-- BEGIN PAGE LEVEL STYLES -->
       <link href="{{asset('assets/pages/css/login-4.min.css')}}" rel="stylesheet" type="text/css" />
@@ -50,22 +44,9 @@
       <!-- END COPYRIGHT -->
       <!-- BEGIN CORE PLUGINS -->
       <script src="{{'js/corePlugins.js'}}"></script>
-      {{-- <script src="{{asset('assets/global/plugins/jquery.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/jquery-ui/jquery-ui.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/js.cookie.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/jquery.blockui.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js')}}" type="text/javascript"></script> --}}
-      
       <!-- END CORE PLUGINS -->
       <!-- BEGIN PAGE LEVEL PLUGINS -->
-      <script src="{{'js/pageLevelScript.js'}}"></script>
-      {{-- <script src="{{asset('assets/global/plugins/jquery-validation/js/jquery.validate.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/jquery-validation/js/additional-methods.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/select2/js/select2.full.min.js')}}" type="text/javascript"></script>
-      <script src="{{asset('assets/global/plugins/backstretch/jquery.backstretch.min.js')}}" type="text/javascript"></script> --}}
-      
+      <script src="{{'js/pageLevelScript.js'}}"></script>      
       <!-- END PAGE LEVEL PLUGINS -->
       <!-- BEGIN THEME GLOBAL SCRIPTS -->
       <script src="{{asset('assets/global/scripts/app.min.js')}}" type="text/javascript"></script>
