1949854ddff60c92a793f38c0ca3c91da2e02b50
JorgeEnriquez1994 <enriquezrodriguezjorgeangel@gmail.com>
2018-07-18 22:34:36 -0500

    add properties
    add properties to models

--- app/Models/City.php
+++ app/Models/City.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    protected $table = 'cities';
-    protected $primarykey = 'id_City';
-    protected $fillable = [];
+   protected $table = 'cities';
+   protected $primarykey = 'id_City';
+   protected $fillable = [];
 }
